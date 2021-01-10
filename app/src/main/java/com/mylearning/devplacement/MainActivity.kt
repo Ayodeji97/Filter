1128430dd6c2f15f2eeaf60e3bf4a12b31fe8b10
Daniel Ayodeji <danielayodeji97@gmail.com>
2021-01-10 08:50:39 +0100

    reference bottom nav in main activity

--- app/src/main/java/com/mylearning/devplacement/MainActivity.kt
+++ app/src/main/java/com/mylearning/devplacement/MainActivity.kt
@@ -2,12 +2,38 @@ package com.mylearning.devplacement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mylearning.devplacement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var ui : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+
+        setUpViews()
+
+        return setContentView(ui.root)
+
+    }
+
+    fun setUpViews () {
+
+        // find the nav controller
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+
+        navController = navHostFragment.findNavController()
+
+        // setting nav controller with bottom navigation view
+        ui.bottomNavView.setupWithNavController(navController)
 
 
     }
+
 }
