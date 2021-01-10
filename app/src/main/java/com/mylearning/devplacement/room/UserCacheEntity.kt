308f6c7c06b3808acad6d2aa4329c3609f5ab258
Daniel Ayodeji <danielayodeji97@gmail.com>
2021-01-10 17:39:54 +0100

    create user cache entity

---
+++ app/src/main/java/com/mylearning/devplacement/room/UserCacheEntity.kt
@@ -0,0 +1,33 @@
+package com.mylearning.devplacement.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "accounts")
+class UserCacheEntity (
+
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    var id : String,
+
+    @ColumnInfo(name = "image")
+    var image : String,
+
+    @ColumnInfo(name = "name")
+    var name : String,
+
+    @ColumnInfo(name = "gender")
+    var gender : String,
+
+    @ColumnInfo(name = "colors")
+    var colors : List<String>,
+
+    @ColumnInfo(name = "countries")
+    var countries : List<String>,
+
+    @ColumnInfo(name = "createAt")
+    var date : String
+
+
+)
