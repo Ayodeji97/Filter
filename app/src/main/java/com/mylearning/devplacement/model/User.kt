ed55b1f56729bdee9da82ed93c58dfafeb998d93
Daniel Ayodeji <danielayodeji97@gmail.com>
2021-01-10 16:29:34 +0100

    added user model

---
+++ app/src/main/java/com/mylearning/devplacement/model/User.kt
@@ -0,0 +1,11 @@
+package com.mylearning.devplacement.model
+
+data class User (
+    var id : String,
+    var name : String,
+    var image : String,
+    var gender : String,
+    var countries : List<String>,
+    var colors : List<String>,
+    var date : String
+    )
---
+++ app/src/main/java/com/mylearning/devplacement/retrofit/UserNetworkEntity.kt
@@ -0,0 +1,36 @@
+package com.mylearning.devplacement.retrofit
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class UserNetworkEntity  (
+    @SerializedName("id")
+    @Expose
+    var id : String,
+
+    @SerializedName("fullName")
+    @Expose
+    var name : String,
+
+    @SerializedName("avatar")
+    @Expose
+    var image : String,
+
+    @SerializedName("gender")
+    @Expose
+    var gender : String,
+
+    @SerializedName("countries")
+    @Expose
+    var countries : List<String>,
+
+    @SerializedName("colors")
+    @Expose
+    var colors : List<String>,
+
+    @SerializedName("createdAt")
+    @Expose
+    var date : String,
+
+
+        )
